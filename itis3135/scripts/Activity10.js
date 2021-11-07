4b7f8fa473f1b69a90b82ea31d6ad57b2831a796
Madison <mmill244@uncc.edu>
2021-11-07 15:25:44 -0500

    Update Activity10.js

--- itis3135/scripts/Activity10.js
+++ itis3135/scripts/Activity10.js
@@ -1,28 +1,27 @@
-"use strict";
+/*"use strict";
 $(document).ready(function() {
 
 
         // preload the image for each link
-        $("a").each( (index, link) => {
+        $("#image_list a").each( (index, link) => {
                 const image = new Image();
                 image.src = link.href;
         });
 
         // set up the event handlers for each link
-        $("a").click( evt => {
+        $("#image_list a").click( evt => {
                 const link = evt.currentTarget;
 
                 // get the image URL and caption for each image and animate the caption
-                $("image").fadeOut(3000);
-                $("caption").fadeOut(3000);
+                $("#image").fadeOut(3000, function() {
+                        $("#image").attr("src", link.href).fadeIn(3000);
+                });
 
-                $("image").attr("src", link.href);
-                $("caption").text(link.title);
+                $("#caption").fadeOut(3000, function() {
+                        $("#caption").text("src", link.href).fadeIn(3000);
+                });
 
-                $("image").fadeIn(3000);
-                $("caption").fadeIn(3000);
-                
-
+        
                 // cancel the default action of each link
                 evt.preventDefault();
                 
@@ -32,3 +31,24 @@ $(document).ready(function() {
         $("li:first-child a").focus();
 
 }); // end ready
+*/
+
+//JQuery from the book pg. 273
+"use strict"
+$(document).ready(() => {
+    $("a").each ( (index, link) => {
+        const image = new Image();
+        image.src = link.href;
+    });
+
+    $("a").click(evt => {
+        const link = evt.currentTarget;
+        $("#main-image").attr("src", link.href);
+        $("#caption").text(link.title);
+        console.log("test");
+        evt.preventDefault();
+    });
+    $("li:first-child a").focus();
+
+});
+
